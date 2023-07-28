a3e45ab627a6aed7feb3edb0fd1bba0623407321
AMULYA1996 <pandaamulya1996@gmail.com>
2023-07-28 17:00:47 +0530

    Main class modified

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigureableApplicationContext run= SpringApplication.run(Application.class, args);
+		run.close();
 	}
 
 }
